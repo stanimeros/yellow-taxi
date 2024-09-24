69345bc973ec6a32b5c5c692d5de777fe5942547
stanimeros <pantelisstanimeros@outlook.com>
2024-09-24 23:10:15 +0300

    trying to beautify ui

--- public/api/apply_coupon.php
+++ public/api/apply_coupon.php
@@ -6,7 +6,7 @@
 
         $price = getPricing($data['startId'], $data['endId'], $data['returnDate'], $data['optionId'], $data['coupons'], $conn);
         $price = number_format($price, 2, '.', '');
-        echo json_encode(['status' => 'success', 'price' => $price, 'coupons_discount' => $coupons_discount]);
+        echo json_encode(['status' => 'success', 'price' => $price);
     }else{
         echo json_encode(['status' => 'failed', 'message' => 'Invalid method']);
     }
--- src/components/form/TripSummary.tsx
+++ src/components/form/TripSummary.tsx
@@ -22,27 +22,31 @@ export const TripSummary: React.FC<TripSummaryProps> = ({ step, globalState }) =
   } = globalState;
 
   return (
-    <Card className="flex flex-col justify-between shadow-lg overflow-hidden flex-shrink w-[300px] h-fit mx-auto">
+    <Card className="flex flex-col justify-between shadow-lg overflow-hidden flex-shrink w-[300px] h-fit mx-auto bg-primary text-white">
       {step>1 && 
         <>
-          <div className='p-4 flex flex-col gap-2'>
-            <h3 className="text-2xl font-bold mb-2 text-primary">Trip Summary</h3>
-            <h4 className='text-l font-bold'>Route {vehicleOption && `- ${vehicleOption.title}`}</h4>
+          <div className="text-2xl font-bold px-2 py-4 border-b border-gray-400">Trip Summary</div>
+          <div className='text-l font-bold p-2'>Route {vehicleOption && `- ${vehicleOption.title}`}</div>
+          <div className='px-4 flex flex-col gap-2'>
             {pickupDateTime && <p>Pickup: {pickupDateTime.toLocaleString(undefined, { dateStyle: 'short', timeStyle: 'short' })}</p>}
             {returnDateTime && <p>Return: {returnDateTime.toLocaleString(undefined, { dateStyle: 'short', timeStyle: 'short' })}</p>}
             <p>{adults} adults{children > 0 && `, ${children} children`}{luggage > 0 ? ` and  ${luggage} luggage pieces` : ` and no luggage`}</p>
-            {(name || email || (phoneCode && phone)) &&
-              <>
-                <h4 className='text-l font-bold'>Book Details</h4>
+          </div>
+          {(name || email || (phoneCode && phone)) &&
+            <>
+              <div className='text-l font-bold p-2'>Book Details</div>
+              <div className='px-4 flex flex-col gap-2'>
                 {name && <p>{name}</p>}
                 {email && <p>{email}</p>}
                 {phoneCode && phone && <p>{`(+${phoneCode}) ${phone}`}</p>}
-              </>
-            }
-            {(ferryName || airplaneName || infantSeats > 0 ||
-            babySeats > 0 || boosterSeats > 0 || bulkyLuggage) &&
-              <>
-                <h4 className='text-l font-bold'>More information</h4>
+              </div>
+            </>
+          }
+          {(ferryName || airplaneName || infantSeats > 0 ||
+          babySeats > 0 || boosterSeats > 0 || bulkyLuggage) &&
+            <>
+              <div className='text-l font-bold p-2'>More information</div>
+              <div className='px-4 flex flex-col gap-2'>
                 {ferryName && <p>Ferry: {ferryName}</p>}
                 {airplaneName && <p>Flight: {airplaneName}</p>}
                 {(infantSeats > 0 || babySeats > 0 || boosterSeats > 0) && (
@@ -54,22 +58,25 @@ export const TripSummary: React.FC<TripSummaryProps> = ({ step, globalState }) =
                   </p>
                 )}
                 {bulkyLuggage && <p>Bulky Luggage: Yes</p>}
-              </>
-            }
-            {notes && 
-              <>
-                <h4 className='text-l font-bold'>Notes</h4>
-                <p>{notes}</p>
-              </>
-            } 
-            {price && (
-              <p className='text-xl mt-6'>
-                <strong>{`Total Price: `}</strong>
-                <span className="text-primary font-bold">{`${price.toFixed(2)}€`}</span>
-              </p>
-            )}
-          </div>
-          {distance && duration && <div>{distance} - {duration}</div>}
+              </div>
+            </>
+          }
+          {notes && 
+            <>
+              <div className='text-l font-bold p-2'>Notes</div>
+              <p className='px-4'>{notes}</p>
+            </>
+          } 
+          {price && (
+            <p className='text-xl font-bold mt-6 p-2'>
+              {`Total Price: ${price.toFixed(2)}€`}
+            </p>
+          )}
+          {distance && duration && (
+            <div className="text-xs text-center font-bold p-2 border-t border-gray-400">
+              Distance: {distance} | Duration: {duration}
+            </div>
+          )}
         </>
       }
       <div className='h-[420px]'>
